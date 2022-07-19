bbce9ef4539cbc69ef7b21a116b0f9925e22a1e6
ankitpareek0141 <ankit.p@yudiz.com>
2022-07-19 22:15:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { CoinbaseWalletSDK } from "@coinbase/wallet-sdk";
+import { useState } from "react";
+
+const providerOptions = {
+  coinbasewallet: {
+    package: CoinbaseWalletSDK, // Required
+    options: {
+      appName: "Coinbase wallet", // Required
+      chainId: 1,
+      rpc: "https://mainnet.infura.io/v3/c66841626ba049a89a3bda5edf5bb634", // Required
+      darkMode: true // Optional. Use dark theme, defaults to false
+    }
+  }
+}
 
 function App() {
+
+  const [ provider, setProvider ] = useState(null);
+
+  async function connectWallet() {
+    try {
+      
+      const web3Modal = new Web3Modal({
+        cacheProvider: false, // optional
+        providerOptions // required
+      });
+
+      const web3ModalInstance = await web3Modal.connect();
+      const web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance);
+      let signer = web3ModalProvider.provider.selectedAddress;
+
+      if(web3ModalProvider) {
+        setProvider(web3ModalProvider);
+        console.log("provider := ", web3ModalProvider);
+        console.log("Selected Address := ", signer);
+      }
+
+    } catch(error) {
+      console.log("Error := ", error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Connect your wallet!
+        </h1>
+        {
+          provider == null ? (
+            <button onClick={connectWallet}>Connect Wallet</button>
+          ) : (
+            <React.Fragment>
+              <p>Connected Address : <strong>{provider.provider.selectedAddress}</strong> </p>
+              <p>Network Id : <strong>{Number(provider.provider.chainId)}</strong> </p>
+            </React.Fragment>
+          )
+        }
       </header>
     </div>
   );
